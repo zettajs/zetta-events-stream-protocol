151037440e1b13dfc31d58ea714629dd1a10b963
Adam Magaluk <AdamMagaluK@gmail.com>
2016-01-13 16:10:21 -0700

    Add specific protocol errors

--- parser.js
+++ parser.js
@@ -1,10 +1,27 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
+var Errors = {
+  'JSONSyntaxError': 'Message is not valid JSON.',
+  'MissingTypeError': 'Message does not contain type.',
+  'InvalidTypeError': 'Invalid type for message.',
+  'MissingParameterError': 'Missing parameter for message type..'
+};
+Object.keys(Errors).forEach(function(errorName) {
+  var tmpMessage = Errors[errorName];
+  Errors[errorName] = function(message) {
+    this.name = errorName;
+    this.message = (message || tmpMessage);
+  }
+  Errors[errorName].prototype = Error.prototype;
+});
+
 var EventStreamParser = module.exports = function() {
   EventEmitter.call(this);  
 };
 util.inherits(EventStreamParser, EventEmitter);
+
+EventStreamParser.Errors = Errors;
 
 EventStreamParser.prototype.add = function(buf) {
   var json = null;
@@ -16,18 +33,23 @@ EventStreamParser.prototype.add = function(buf) {
       json = JSON.parse(buf);  
     }
   } catch(e) {
-    self.emit('error', e, buf);  
+    self.emit('error', new Errors.JSONSyntaxError(e.message), buf);
     return;
   }
-
-  if (this.validate(json)) {
+  
+  var res = this.validate(json);
+  if (res === true) {
     this.emit(json.type, json);
   } else {
-    this.emit('error', new Error('Message validation failed.'), json);
+    this.emit('error', res, json);
   }
 };
 
 EventStreamParser.prototype.validate = function(json) {
+  if (json.type === undefined) {
+    return new Errors.MissingTypeError();
+  }
+
   var properties = {
     'subscribe': { topic: 'string' },
     'unsubscribe': { subscriptionId: 'number' },
@@ -38,16 +60,17 @@ EventStreamParser.prototype.validate = function(json) {
   }
 
   var keys = properties[json.type];
-  var valid = true; 
-  if(keys) {
-    Object.keys(keys).forEach(function(key) {
-      if(typeof json[key] !== keys[key]) {
-        valid = false;
-      }  
-    });
-  } else {
-    return false;  
+  if (!keys) {
+    return new Errors.InvalidTypeError();
   }
 
-  return valid;
+  var valid = Object.keys(keys).every(function(key) {
+    return (typeof json[key] === keys[key]);
+  });
+
+  if (!valid) {
+    return new Errors.MissingParameterError();
+  } else {
+    return true;
+  }
 };
